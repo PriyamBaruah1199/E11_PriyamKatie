55b64f2faf5c57a3895bf4af2efefd8643a3c757
PriyamBaruah1199 <158365812+PriyamBaruah1199@users.noreply.github.com>
2024-01-31 16:13:23 -0800

    Add files via upload

---
+++ weather_daq.py
@@ -0,0 +1,29 @@
+import adafruit_bme680
+import time
+import board
+
+# Create sensor object
+i2c = board.I2C() # Uses board.SCL and board.SDA
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+# Change this to match the location's pressure (hPa) at sea level
+bme680.sea_level_pressure = 1013.25
+
+start = time.time()
+print(start)
+print("")
+
+while True:
+	print("\nTemperature: %0.1f C" % bme680.temperature)
+	print("Gas: %d ohm" % bme680.gas)
+	print("Humidity: %0.1f %%" % bme680.relative_humidity)
+	print("Pressure: %0.3f hPa" % bme680.pressure)
+	print("Altitude = %0.2f meters" % bme680.altitude)
+	
+	now = time.time() - start
+	print(now, " seconds have passed")
+	
+	time.sleep(2)
+	
+	if time > 20:
+		break 
