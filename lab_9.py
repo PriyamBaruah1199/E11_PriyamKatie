d2251a9ae263c89fb90e0bd2af6bba3b37b1871a
PriyamBaruah1199 <158365812+PriyamBaruah1199@users.noreply.github.com>
2024-03-13 16:24:09 -0700

    Update lab_9.py

--- lab_9.py
+++ lab_9.py
@@ -25,8 +25,6 @@ try:
     GPIO.setup(channel, GPIO.IN)
     GPIO.add_event_detect(channel, GPIO.BOTH, callback=my_callback)
 
-    message = raw_input('\nPress any key to exit.\n')
-
 finally:
     GPIO.cleanup()
 
