ae27df479be85ee370cd7b86fc61bef3b71a3f90
PriyamBaruah1199 <158365812+PriyamBaruah1199@users.noreply.github.com>
2024-03-13 16:29:33 -0700

    Update lab_9.py

--- lab_9.py
+++ lab_9.py
@@ -11,31 +11,32 @@ Original file is located at
 
 import RPi.GPIO as GPIO
 import datetime
+channel = 16
 
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
 
-if GPIO.input(16):
+if GPIO.input(channel):
     print('Input was HIGH')
 else:
     print('Input was LOW')
 
-while GPIO.input(16) == GPIO.LOW:
+while GPIO.input(channel) == GPIO.LOW:
     time.sleep(0.01)  # wait 10 ms to give CPU chance to do other things
 
-channel = GPIO.wait_for_edge(16, GPIO_RISING, timeout=5000)
+channel = GPIO.wait_for_edge(channel, GPIO_RISING, timeout=5000)
 if channel is None:
     print('Timeout occurred')
 else:
     print('Edge detected on channel', channel)
 
 
-GPIO.add_event_detect(16, GPIO.RISING)  # add rising edge detection on a channel
+GPIO.add_event_detect(channel, GPIO.RISING)  # add rising edge detection on a channel
 do_something()
 if GPIO.event_detected(16):
     print('Button pressed')
 
 
-def my_callback(16):
+def my_callback(channel):
     print('This is a edge event callback function!')
     print('Edge detected on channel %s'%channel)
     print('This is run in a different thread to your main program')
