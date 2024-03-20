1f9d1381ce942ce21c46e9f4cacb5d2cafcf0e7f
PriyamBaruah1199 <158365812+PriyamBaruah1199@users.noreply.github.com>
2024-03-20 14:55:56 -0700

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -1,21 +1,20 @@
 import RPi.GPIO as GPIO
 import datetime
- 
+
 def my_callback(channel):
     if GPIO.input(channel) == GPIO.HIGH:
         print('\n▼  at ' + str(datetime.datetime.now()))
     else:
         print('\n ▲ at ' + str(datetime.datetime.now())) 
- 
+
 try:
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
-
+    GPIO.setup(36, GPIO.IN)
+    GPIO.add_event_detect(36, GPIO.BOTH, callback=my_callback)
+ 
+    message = raw_input('\nPress any key to exit.\n')
  
 finally:
     GPIO.cleanup()
-
-my_callback(16)
-
+ 
 print("Goodbye!")
