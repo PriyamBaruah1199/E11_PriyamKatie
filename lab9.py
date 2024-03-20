85d88e026d8b70832bc737e22673f583c3c90854
sundstromkatie <158365863+sundstromkatie@users.noreply.github.com>
2024-03-20 16:23:02 -0700

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -10,43 +10,18 @@ def my_callback(channel):
     print('\n▼  at ' + str(datetime.datetime.now()))
     counts = counts + 1 
 
-#def input_available(timeout):
-    # Check if input is available within the specified timeout
-    #start_time = time.time()
-    # while time.time() - start_time < timeout:
-    #run_time = int(sys.argv[1])
-    #while time.time() - start_time < run_time:
-       # if sys.stdin in select.select([sys.stdin], [], [], timeout)[0]:
-          #  return True
-    #return False
-
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(16, GPIO.IN)
-    GPIO.add_event_detect(16, GPIO.FALLING, callback=my_callback)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.IN)
+GPIO.add_event_detect(16, GPIO.FALLING, callback=my_callback)
  
-    start_time = time.time()
-    run_time = int(sys.argv[1])
-    filename = sys.argv[2]
+start_time = time.time()
+run_time = int(sys.argv[1])
+filename = sys.argv[2]
 
-    while time.time() - start_time < run_time:
-        # Check if the timeout has been reached
-        # if time.time() - start_time > timeout:
-            # break
-
-        # Check for user input to exit
-        # if input_available(0.1):
-            # break
-
-        # Add a small delay to reduce CPU usage
-        time.sleep(10)
-        print(counts,"counts read")
-        counts = 0
+while time.time() - start_time < run_time:
+    time.sleep(10)
+    print(counts,"counts read")
+    counts = 0
     
-finally:
-    GPIO.cleanup()
-# print(counts,"counts read")
-print("press enter to exit")
-input()
- 
-print("Goodbye!")
+
+
