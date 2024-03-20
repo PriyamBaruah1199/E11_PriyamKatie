af2445ce84949e2acb07a7582ead83d0874a9e77
PriyamBaruah1199 <158365812+PriyamBaruah1199@users.noreply.github.com>
2024-03-20 16:27:19 -0700

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -1,33 +1,41 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
-import sys 
-import select 
+import sys
+import select
+import csv
 
-meta_data = ["Counts","Time Stamp"]
-dwriter.writerow(meta_data)
-print(meta_data)
+meta_data = ["Counts", "Time Stamp"]
 
-counts = 0 
-def my_callback(channel):
-    global counts
-    print('\n▼  at ' + str(datetime.datetime.now()))
-    counts = counts + 1 
+# Open a CSV file for writing
+with open(sys.argv[2], 'w', newline='') as csvfile:
+    dwriter = csv.writer(csvfile)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.IN)
-GPIO.add_event_detect(16, GPIO.FALLING, callback=my_callback)
- 
-start_time = time.time()
-run_time = int(sys.argv[1])
-filename = sys.argv[2]
+    # Write the metadata to the CSV file
+    dwriter.writerow(meta_data)
+    print(meta_data)
 
-while time.time() - start_time < run_time:
-    time.sleep(10)
-    print(counts,"counts read")
-    data = [counts]
-    dwriter.writerow(data)
     counts = 0
-    
 
+    def my_callback(channel):
+        global counts
+        print('\n▼  at ' + str(datetime.datetime.now()))
+        counts += 1
 
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(16, GPIO.IN)
+    GPIO.add_event_detect(16, GPIO.FALLING, callback=my_callback)
+
+    start_time = time.time()
+    run_time = int(sys.argv[1])
+
+    while time.time() - start_time < run_time:
+        time.sleep(10)
+        print(counts, "counts read")
+        
+        # Write the counts and timestamp to the CSV file
+        data = [counts, str(datetime.datetime.now())]
+        dwriter.writerow(data)
+        counts = 0
+
+print("Data writing complete.")
