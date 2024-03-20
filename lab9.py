ec841fcca2e548a1bc26bc6e258235fbf8a7c901
PriyamBaruah1199 <158365812+PriyamBaruah1199@users.noreply.github.com>
2024-03-20 15:33:08 -0700

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -8,6 +8,7 @@ counts = 0
 def my_callback(channel):
     if GPIO.input(channel) == GPIO.HIGH:
         print('\n▼  at ' + str(datetime.datetime.now()))
+        counts = counts + 1 
     else:
         print('\n ▲ at ' + str(datetime.datetime.now())) 
 
@@ -22,7 +23,7 @@ def input_available(timeout):
 try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(16, GPIO.IN)
-    GPIO.add_event_detect(16, GPIO.BOTH, callback=my_callback, counts = counts + 1)
+    GPIO.add_event_detect(16, GPIO.BOTH, callback=my_callback)
 
     timeout = 10  # 2 minutes in seconds
     start_time = time.time()
